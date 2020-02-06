8c35c1762efbe01bbdd8912f08691f7cf3a6ec37
Pradip <Pradip@192.168.0.30>
2020-02-06 22:07:02 +0530

    Added logging

--- src/main/java/com/shopping/services/RegisterServiceImpl.java
+++ src/main/java/com/shopping/services/RegisterServiceImpl.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Service;
 import com.shopping.entities.User;
 import com.shopping.repositories.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Scope(value = "session")
 @Service
+@Slf4j
 public class RegisterServiceImpl implements RegisterService {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	public User save(final User user) {
+		log.info("Inside Register Service...");
 		return userRepository.save(user);
 	}
 }
