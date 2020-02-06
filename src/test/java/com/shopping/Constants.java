946e073e67948b3df05cd034f67d7b373eea3ddc
Kulbhushan.Saxena <Kulbhushan.Saxena@thomsonreuters.com>
2020-02-06 20:46:33 +0530

    Added TestCases for Login Service

--- src/test/java/com/shopping/Constants.java
+++ src/test/java/com/shopping/Constants.java
@@ -2,7 +2,7 @@ package com.shopping;
 
 import java.util.Date;
 
-public class Constants {
+public interface Constants {
 	public static final String FULL_NAME = "fullname";
 	public static final String EMAIL = "user@test.com";
 	public static final Date DOB = new Date();
@@ -10,4 +10,8 @@ public class Constants {
 	public static final String PASSWORD = "password";
 	public static final String COUNTRY_CODE = "+91";
 	public static final String MOBILE_NUMBER = "1234567890";
+	
+	public static final String ADMIN = "admin";
+	public static final String USER_NOT_FOUND = "User Does not exist";
+	
 }
