ddee008b54b203044cb79a7fb4d6059a02ca6d57
Pradip Rachamalla <Pradip.Rachamalla@thomsonreuters.com>
2020-02-06 15:01:16 +0530

    Added Repos

---
+++ src/main/java/com/shopping/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.shopping.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopping.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+}
